bdf57e4bda090b6e04cd5981737ee87add607efc
Atomic-Gamer <72369570+Atomic-Gamer@users.noreply.github.com>
2025-09-09 13:25:09 +0530

    s

--- script.js
+++ script.js
@@ -126,3 +126,23 @@
     });
   });
 })();
+
+
+function setHeights(){
+  const about = document.getElementById('about');
+  // offsetHeight gives the **unscaled** layout height (what we want)
+  document.documentElement.style.setProperty('--about-h', about.offsetHeight + 'px');
+}
+window.addEventListener('load', setHeights);
+window.addEventListener('resize', setHeights);
+
+
+const width = window.innerWidth;
+
+if (width <= 1200) {
+  document.body.classList.add('breakpoint-sm');
+} else if (width <= 1630) {
+  document.body.classList.add('breakpoint-md');
+} else {
+  document.body.classList.add('breakpoint-lg');
+}
